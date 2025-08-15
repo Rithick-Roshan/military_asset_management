e6536dc293284d08f1009311c4326933deafd139
Rithick Roshan <rithick1887@gmail.com>
2025-08-15 12:09:33 +0530

    database tables are created

---
+++ database/Database.sql
@@ -0,0 +1,90 @@
+create database military_asset_management;
+use military_asset_management;
+
+create table users(
+      user_id int auto_increment primary key,
+      username varchar(50) unique not null,
+      email varchar(100) unique not null,
+      password_hash varchar(255) not null,
+      role enum('Admin','Base_Commander','Logistics_Officer') not null,
+      base_id int,
+	  status boolean default true
+);
+
+create table bases(
+      base_id int auto_increment primary key,
+      base_name varchar(100) not null,
+      base_code varchar(10) unique not null,
+      location varchar(200) not null,
+      commander_id int
+);
+
+
+create table equipment_types(
+    equipment_type_id int auto_increment primary key,
+    epuipment_name varchar(100) not null,
+    category enum('Weapons','Vehicles','Ammunition','Electronics','Medical','Communications','Others') not null,
+    base_id int 
+);
+
+create table assets(
+    asset_id int auto_increment primary key,
+    asset_serial_number varchar(50) unique not null,
+    equipment_type_id int not null,
+    current_status enum('Available', 'Assigned', 'Expended', 'In_Transit', 'Under_Maintenance', 'Decommissioned') default 'Available',
+    purchase_date date,
+    purchase_price decimal(12,2),
+    condition_status enum('Excellent', 'Good', 'Fair', 'Poor', 'Damaged') default 'Good',
+    manufacturer varchar(100),
+    model varchar(100),
+    year_manufactured year,
+    warranty_expiry date,
+    available int,
+    status enum('Sufficient','In-sufficient') default 'Sufficient',
+    base_id int
+);
+
+create table transfers(
+     transfer_id int auto_increment primary key,
+     asset_id int not null,
+     from_base_id int not null,
+     to_base_id int not null,
+     status varchar(10) not null,
+     transfer_date date not null,
+     quantity int not null check(quantity>0)
+);
+
+create table purchases(
+   purchase_id int auto_increment primary key,
+   purchase_order_number varchar(50) unique not null,
+   asset_id int not null,
+   base_id int not null,
+   equipment_type_id int not null,
+   quantity int not null check(quantity>0),
+    unit_price decimal(10,2) not null check (unit_price > 0),
+   total_amount decimal(12,2) not null check (total_amount > 0),
+    purchase_date date not null,
+	status enum('Pending', 'Approved', 'Ordered', 'Delivered', 'Cancelled') default 'Pending',
+    supplier varchar(50) not null
+);
+
+create table assignments(
+    assignment_id int auto_increment primary key,
+    asset_id int not null,
+    base_id int not null,
+    Mission varchar(200) not null,
+    status enum('Pending','Failed','Compeleted') default 'Pending'
+);
+
+create table expenditures(
+     expenditure_id int auto_increment primary key,
+     asset_id int not null,
+     base_id int not null,
+     reason varchar(200) not null,
+     expenditure_date date not null
+);
+
+
+
+
+
